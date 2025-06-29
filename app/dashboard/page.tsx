d6cc02b4cb0b857bcf263cf582f78c2f08e51665
Moein Hoseini <Moein.devht@gmail.com>
2025-06-29 13:13:24 +0330

    lint the project

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,3 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-
 export default async function DashboardPage() {
   return <h1>Welcome to the Dashboard</h1>;
 }
